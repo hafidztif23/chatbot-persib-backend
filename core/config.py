298d2fa370bf900597699888014ca22502a15ea8
Rifqi Yudhistira Sumantri <rifqiyudhistira53@gmail.com>
2026-07-09 13:42:47 +0700

    Update bahasa

--- core/config.py
+++ core/config.py
@@ -8,11 +8,4 @@ API_BASE_URL = os.getenv("API_BASE_URL")
 CHATBOT_NAME = os.getenv("CHATBOT_NAME", "Asisten Persib")
 
 
-LANGUAGE_INSTRUCTION = (
-    "Identifikasi bahasa yang digunakan oleh user dalam pertanyaannya. "
-    "Jawablah menggunakan bahasa yang sama dengan pertanyaan user tersebut, "
-    "tetapi batasi pilihan bahasa jawaban HANYA ke dalam tiga bahasa ini: "
-    "1) Bahasa Indonesia, 2) English (Bahasa Inggris), atau 3) Bahasa Sunda. "
-    "Jika user bertanya menggunakan bahasa selain ketiga bahasa tersebut, jawablah menggunakan Bahasa Indonesia. "
-    "Jawab selalu dalam bahasa yang formal tapi tetap ramah dan mudah dipahami. Gunakan bahasa sehari-hari yang natural, tidak kaku, dan tidak terlalu baku. Sesuaikan gaya bahasa dengan konteks percakapan agar terasa lebih personal dan menyenangkan bagi pengguna."
-)
+LANGUAGE_INSTRUCTION = f"""Jawab selalu dalam bahasa sesuai dengan bahasa yang digunakan oleh user lalu jawab yang formal tapi tetap ramah dan mudah dipahami. Gunakan bahasa sehari-hari yang natural, tidak kaku, dan tidak terlalu baku. Sesuaikan gaya bahasa dengan konteks percakapan agar terasa lebih personal dan menyenangkan bagi pengguna."""
